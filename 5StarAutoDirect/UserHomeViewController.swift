088c1aed2b2083f4f00241a1fd7790e7e4a09ba9
MikeDCastillo <mikecastillouvu@gmail.com>
2017-10-22 17:17:01 -0600

    Uses segue handling protocol

--- 5StarAutoDirect/UserHomeViewController.swift
+++ 5StarAutoDirect/UserHomeViewController.swift
@@ -11,45 +11,42 @@ import Firebase
 
 class UserHomeViewController: UIViewController {
     
-    static let shared = UserHomeViewController()
     
     var user: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         NotificationCenter.default.addObserver(self, selector: #selector(newMessageReceived), name: .messagesUpdated, object: nil)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        if Auth.auth().currentUser != nil {
-        }
-    }
-    
     @IBAction func signOutButtonTapped(_ sender: Any) {
-    try! Auth.auth().signOut()
+        try! Auth.auth().signOut()
         navigationController?.popViewController(animated: true)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "userHomeVCToMessageConvoVC" {
-            
-            let selectedUser = UserHomeViewController.shared.user
-            if let detailVC = segue.destination as? MessageConvoViewController {
-                detailVC.customer = selectedUser
-            }
-            
-        } else {
-            if segue.identifier == "toCarDetail" {
-                let selectedUser = CarDetailViewController.shared.user
-                if let detailVC = segue.destination as? CarDetailViewController {
-                    detailVC.user = selectedUser
-                }
-            }
-        }
-    }
     
     func newMessageReceived() {
         showNotificationBanner()
     }
+}
+
+
+extension UserHomeViewController: SegueHandling {
+    
+    enum SegueIdentifier: String {
+        case userHomeVCToMessageConvoVC
+        case toCarDetail
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segueIdentifier(for: segue) {
+        case .userHomeVCToMessageConvoVC:
+            UserController.shared.selectedUser = UserController.shared.currentUser
+        case .toCarDetail:
+            let selectedUser = CarDetailViewController.shared.user
+            guard let detailVC = segue.destination as? CarDetailViewController else { return }
+            detailVC.user = selectedUser
+        }
+    }
+
 }
